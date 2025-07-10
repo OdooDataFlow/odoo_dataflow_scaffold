98d140928957af1d492d641b4c89bfd4fe154d84
bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
2025-07-10 14:23:33 +0200

    [FIX] bool mapper syntax

--- odoo_dataflow_scaffold.py
+++ odoo_dataflow_scaffold.py
@@ -739,7 +739,7 @@ class ModelField:
         elif self.type in ('integer', 'float', 'monetary'):
             return f"mapper.num('{self.get_name()}')"
         elif self.type in ('boolean'):
-            return f"mapper.bool_val('{self.get_name()}', true_vals=true_values, false_vals=false_values)"
+            return f"mapper.bool_val('{self.get_name()}', true_values=true_values, false_values=false_values)"
         elif self.type in ('datetime'):
             return f"mapper.val('{self.get_name()}', postprocess=lambda x: datetime.strptime(x, 'CSV_DATE_FORMAT').strftime('%%Y-%%m-%%d 00:00:00') if x else '')"
         elif self.type in ('binary'):
